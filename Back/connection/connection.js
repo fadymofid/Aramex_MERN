b6890e03b5aa5fb0a38a8c17f2b786e4ebd9fbe2
fady <108768356+fadymofid@users.noreply.github.com>
2024-12-01 22:17:05 +0200

    adding docker files

--- Back/connection/connection.js
+++ Back/connection/connection.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// Use the container name 'mongodb' instead of localhost
+const mongoURI = "mongodb://admin:admin@mongodb:27017";
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    
+const connectDB = () => {
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 }
 
-module.exports = connectDB
+// Call connectDB function to establish the connection
+connectDB();
+
+module.exports = connectDB;
